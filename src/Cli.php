70cdd17d4b0acabd8818929a97f52739fcb39b16
Andrey V <vinyaykin@gmail.com>
2018-04-13 16:32:46 +0300

    1.3.0 stage 4

--- src/Cli.php
+++ src/Cli.php
@@ -3,9 +3,11 @@ namespace BrainGames\Cli;
 
 use function \cli\line;
 
-function run()
+function run($description = NULL)
 {
     line('Welcome to the Brain Game!');
+    if($description) line($description);
     $name = \cli\prompt('May I have your name?');
-    line("Hello, %s!", $name);
+    line("Hello, %s!\n", $name);
+    return $name;
 }
---
+++ src/Lib.php
@@ -0,0 +1,21 @@
+<?php
+namespace BrainGames\Lib;
+
+use function \cli\line;
+
+function brainGamesOne($name){
+  for ($i=0; $i < 3; $i++) {
+    $x = rand(1,100);
+    line("Question: %s", $x);
+    $answer = \cli\prompt('Your answer');
+    $x%2 == 0 ? $cor_answ = "yes" : $cor_answ ="no";
+    if ( $answer == $cor_answ){
+      line('Correct!');
+    }else{
+      line("'$answer' is wrong answer ;(. Correct answer was '$cor_answ'.");
+      line("Let's try again, $name!");
+      exit;
+    }
+  }
+  line("Congratulations, $name!");
+}
