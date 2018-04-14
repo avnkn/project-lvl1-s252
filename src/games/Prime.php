59c90c99c40a586e3a22deca0baeddaddabaff72
Andrey V <vinyaykin@gmail.com>
2018-04-14 22:08:03 +0300

    1.8.0 stage 9

---
+++ src/games/Prime.php
@@ -0,0 +1,22 @@
+<?php
+namespace BrainGames\Games\Prime;
+
+use function BrainGames\LogicGame\render;
+
+function game()
+{
+    $desc = 'Answer "yes" if number prime otherwise answer "no".';
+    $func = function () {
+        $a = rand(1, 100);
+        $i_m = sqrt($a);
+        for ($i=2; $i<$i_m; ++$i) {
+            if ($a % $i == 0) {
+                $arr_data[$a] = "no";
+                return $arr_data;
+            }
+        }
+        $arr_data[$a] = "yes";
+        return $arr_data;
+    };
+    render($desc, $func);
+}
