70a4c8a8f392d283f222f66347b4a7360fa56957
Andrey V <vinyaykin@gmail.com>
2018-04-14 20:10:36 +0300

    1.6.5 new algoritm stage 6

--- src/games/Gcd.php
+++ src/games/Gcd.php
@@ -3,21 +3,18 @@ namespace BrainGames\Games\Gcd;
 
 use function BrainGames\LogicGame\render;
 
+function gcd($a, $b)
+{
+    return $a == 0 ? abs($b) : gcd($b % $a, $a);
+}
+
 function game()
 {
     $desc = "Find the greatest common divisor of given numbers.";
     $func = function () {
         $arg1 = rand(1, 100);
         $arg2 = rand(1, 100);
-        $arg1 > $arg2 ? $arg_min = $arg2 : $arg_min = $arg1;
-        for ($j=0; $j < $arg_min; $j++) {
-            $del = $arg_min - $j;
-            if ($arg1%$del == 0 and $arg2%$del == 0) {
-                $cor_answ = $del;
-                $arg_min = 0;
-            }
-        }
-        $arr_data["$arg1 $arg2"] = $cor_answ;
+        $arr_data["$arg1 $arg2"] = gcd($arg1, $arg2);
         return $arr_data;
     };
     render($desc, $func);
